7b05033ddc16a584202f53df670d8cece8874fad
1DiEgo11 <sona.egor23@gmail.com>
2023-04-25 20:41:40 +1000

    Добавьте файлы проекта.

---
+++ DataBase/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Server_Client;
+
+//чтение файла
+static List<User> bd()
+{
+    var bd = new List<User>();
+    using (StreamReader fstream = new StreamReader("note2.txt"))
+    {
+        while (true)
+        {
+            User user = new User();
+            string textFromFile = fstream.ReadLine();
+            if (textFromFile == null) break;
+            user.id = int.Parse(textFromFile.Substring(0, 1));
+            textFromFile = textFromFile.Substring(2);
+            foreach (char i in textFromFile)
+            {
+                if (i != ' ')
+                {
+                    user.login += i;
+                    textFromFile = textFromFile.Substring(1);
+                }
+                else
+                {
+                    user.password = textFromFile.Substring(1, textFromFile.Length - 2);
+                    user.isAdmin = int.Parse(textFromFile.Substring(textFromFile.Length - 1));
+
+                    break;
+                }
+            }
+            bd.Add(user);
+        }
+    }
+    return bd;
+}
+
+////запись в файл
+static void Write(List<User> list)
+{
+    using (StreamWriter fstream = new StreamWriter("note2.txt"))
+    {
+        foreach (var us in list)
+        {
+
+            fstream.WriteLine($"{us.id} {us.login} {us.password} {us.isAdmin}");
+        }
+    }
+}
+
+
+
+TcpListener server = new TcpListener(IPAddress.Any, 7000);
+server.Start();
+
+
+string s = "Привет!";
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    NetworkStream stream = client.GetStream();
+
+    ReceivingAndSending.Sending(stream, s);
+
+
+    string request = ReceivingAndSending.Receiving(stream);
+    Console.WriteLine("Got req: " + request);
+}
+
+server.Stop();
+
+public class User
+{
+    public string login = "";
+    public string password = "";
+    public int isAdmin;
+    public int id;
+};
+
---
+++ Server-Client/ReceivingAndSending.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Server_Client
+{
+    public class ReceivingAndSending
+    {
+        public static string Receiving(NetworkStream stream)
+        {
+            byte[] bytes = new byte[1024];
+            int lenght = stream.Read(bytes, 0, bytes.Length);
+            string request = Encoding.UTF8.GetString(bytes, 0, lenght);
+            return request;
+        }
+
+        public static void Sending(NetworkStream stream, string s)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+        }
+    }
+}
